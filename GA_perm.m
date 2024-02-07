b4d5987b6d1afbc948bf1760ae03652a4de7e36a
Alexandercheng-rsch <alexandercheng102@gmail.com>
2024-02-07 00:47:02 +0000

    this is taking so long

--- GA_perm.m
+++ GA_perm.m
@@ -4,14 +4,15 @@ num_cities = length(inputcities)
 %
 %GA variables
 crossover_prob = 0.6;
-num_ind = 5; %no. solutions
+num_ind = 6; %no. solutions
 mutation_prob = 1/num_cities;
+max_iter = 1000
 %%looping to make population matrix
-pop = zeros([2,48,5])
-inputcities_dup = repmat(inputcities,[1,1,5])
-for i = 1:5
-    p = randperm(num_cities)
-    pop(:,:,i) = inputcities_dup(:,p,i)
+pop = zeros([2,48,num_ind]);
+inputcities_dup = repmat(inputcities,[1,1,num_ind]);
+for i = 1:num_ind
+    p = randperm(num_cities);
+    pop(:,:,i) = inputcities_dup(:,p,i);
 end
 
 %%inital solutions
@@ -30,10 +31,30 @@ termination = false;
 t = 1;
 %
 
-while termination==false;
+while termination==false
 
-    parents = pop(:,:,num_parents)%truncation selection;
+    parents = pop(:,:,num_parents);%truncation selection;
     offspring = parents;
+
+    %%crossover
+    for i = 1:floor(num_parents/2)
+        if rand(1) < crossover_prob
+            idx = randi([1 num_parents],2,1);
+            cross_point = randi([1 num_cities]);
+            offspring(:,cross_point+1,idx(1,:)) = parents(:,cross_point+1,idx(2,:));
+            offspring(:,cross_point+1,idx(2,:)) = parents(:,cross_point+1,idx(1,:));
+        end
+    end
+    %%
+
+
+    t = t+1;
+    plotcities(offspring)
+    if t>max_iter
+        termination=true;
+end
+
+
 
 end
 
