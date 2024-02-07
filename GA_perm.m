b42b966c9e6c51f57810ef2109e8d48acda94c3c
Alexandercheng-rsch <alexandercheng102@gmail.com>
2024-02-07 09:11:56 +0000

    semi-finished GA

--- GA_perm.m
+++ GA_perm.m
@@ -6,7 +6,7 @@ num_cities = length(inputcities)
 crossover_prob = 0.6;
 num_ind = 6; %no. solutions
 mutation_prob = 1/num_cities;
-max_iter = 1000
+max_iter = 500;
 %%looping to make population matrix
 pop = zeros([2,48,num_ind]);
 inputcities_dup = repmat(inputcities,[1,1,num_ind]);
@@ -41,20 +41,31 @@ while termination==false
         if rand(1) < crossover_prob
             idx = randi([1 num_parents],2,1);
             cross_point = randi([1 num_cities]);
-            offspring(:,cross_point+1,idx(1,:)) = parents(:,cross_point+1,idx(2,:));
-            offspring(:,cross_point+1,idx(2,:)) = parents(:,cross_point+1,idx(1,:));
+            offspring(:,cross_point+1,idx(1,:),:) = parents(:,cross_point+1,idx(2,:),:);
+            offspring(:,cross_point+1,idx(2,:),:) = parents(:,cross_point+1,idx(1,:),:);
         end
     end
-    %%
+    %%mutation 
+    for j = 1:num_cities
+        if rand(1) < mutation_prob
+            idx = randi(num_cities,1);
+            offspring(:,idx,:) = parents(:,j,:);
+        end
+    end
+    
+    temp_pop = cat(3,pop,offspring);
+    [fitness, idx] = sort(distance(temp_pop),'descend');
+    pop = temp_pop(:,:,idx(1:num_ind));
+    plotcities(pop(:,:,1))
+    
+
 
 
     t = t+1;
-    plotcities(offspring)
     if t>max_iter
         termination=true;
 end
-
-
+best_tour = pop(:,:,1);
 
 end
 
